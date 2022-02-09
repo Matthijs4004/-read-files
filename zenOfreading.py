5b8269afe04ee655e124b46b3c5055db2539d129
Matthijs4004 <matthijs4004@gmail.com>
2022-02-09 11:19:27 +0100

    zenOfreading geupdate voor opdracht 2

--- zenOfreading.py
+++ zenOfreading.py
@@ -1,7 +1,6 @@
 import time
 
-file = open("C:\Projecten\Software Dev\Mapje 9\-read-files\README.md", "r")
-
-for x in file:
-    time.sleep(1)
-    print(x)
+with open("C:\Projecten\Software Dev\Mapje 9\-read-files\README.md", "r") as file:
+    for x in file:
+        time.sleep(1)
+        print(x, end="")
